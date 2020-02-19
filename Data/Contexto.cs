719d36bb42ccde08ca604bb5c12f07697850a6be
Erick J. Gutierrez <50554753+erickJ27@users.noreply.github.com>
2020-02-19 15:28:16 -0300

    Add project files.

---
+++ Data/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace primerparcial19.Data
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder.UseSqlite(@"Data source=Database/primerparcial.db"));
+        }
+    }
+}
